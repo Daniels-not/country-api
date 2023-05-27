085c9a1163e06bd4ab5bcdaad57730351ad24284
Daniels-not <ramydcampusano@hotmail.com>
2023-05-27 16:59:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Header'
+
+import {useState} from 'react';
+import Countries from './Countries';
+import Country from './Country';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import useFetch from './reusable/useFetch.js';
 
 function App() {
+
+  const [inputField , setInputField ] = useState(undefined);
+  const [search,setSearch] = useState(undefined);
+  const [filtra,setFilter] = useState("All");
+  const {data} = useFetch("https://restcountries.com/v2/all")
+
+  const handleSubmit = (e) => {
+
+      e.preventDefault();
+
+      setSearch(inputField);
+
+  }
+
+  const handleSelect = (e) => {
+
+    setFilter(e.target.value)
+
+    setSearch(undefined);
+
+    setInputField('');
+  }
+
+  const getCountryName = (code) => {
+
+    let countryName;
+
+    const country = data.filter((element)=>{
+      return element.alpha3Code === code;
+    })  
+
+    countryName = country[0].name
+
+    return countryName;
+  }
+
+  const numberWithCommas = ( number => {
+    return number
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g,",");
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <div className="container">
+                <form onSubmit={handleSubmit}>
+                  <div className="inputField">
+                    
+                    <input 
+                      type="search"
+                      placeholder = "Search for a country..."
+                      value={inputField}
+                      onChange={(e)=>{
+                        setInputField(e.target.value)
+                        setSearch(e.target.value)
+                        }
+                      }
+                    />
+                    <i className="fas fa-search"></i>
+                  </div>
+                  <select id="region" name="region" onChange={handleSelect}>
+                    <option value="All" defaultValue>All</option>
+                    <option value="Africa">Africa</option>
+                    <option value="Americas">America</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Oceania">Oceania</option>
+                  </select>
+                </form>
+              </div>
+              <Countries filtra={filtra} input={search} numberWithCommas={numberWithCommas}/>
+            </Route> 
+            <Route path="/:countryName" children={<Country numberWithCommas={numberWithCommas} getCountryName={getCountryName}/>} />
+          </Switch>
+        </div>
+      </Router>
+    )
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Header = () => {   
+
+    const [theme,setTheme] = useState('light');
+    document.documentElement.className = localStorage.getItem("themeStorage") ? localStorage.getItem("themeStorage"):theme;
+    document.body.classList.remove("light")
+
+    const handleSwitcher = () => {
+
+        if(theme==="light"){
+
+         setTheme("dark")
+
+         document.documentElement.className = theme     
+
+        }else{
+
+         setTheme("light")
+
+         document.documentElement.className = theme
+
+        }
+
+        localStorage.setItem("themeStorage",theme);
+
+    }
+
+    return ( 
+
+        <div className="header">
+            <div className="container">
+                <h1>Where in the world?</h1>
+                <span onClick={handleSwitcher}><i className="fas fa-moon"></i>Dark mode</span>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
